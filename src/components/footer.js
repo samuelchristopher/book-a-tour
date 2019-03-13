1795f6c677fc7377ad2f534a35fb905f2371ad46
Samuel Paul Christopher <thesamchris@gmail.com>
2019-03-13 14:56:07 +0800

    static: footer

---
+++ src/components/footer.js
@@ -0,0 +1,38 @@
+import React from "react"
+import styles from "./footer.module.css"
+
+const Footer = () => (
+  <footer>
+    <div className={styles.footer__logo} />
+    <div className={styles.footer__partnersContainer}>
+      <p className={styles.footer__partnersHeading}>
+        grace travel <br />
+        agency partners
+      </p>
+      <div className={styles.footer__partnersLinksContainer}>
+        <div className={styles.partnersLinks__row}>
+          <a href="http://www.gracebrunei.com/clubmed/">Club Med</a>
+          <a href="http://www.gracebrunei.com/koreanair/">Korean Air</a>
+        </div>
+        <div className={styles.partnersLinks__row}>
+          <a href="http://www.gracebrunei.com/bangkokhospital/">
+            Bangkok Hospital
+          </a>
+          <a href="http://www.gracebrunei.com/bangkokairways/">Bangkok Air</a>
+        </div>
+      </div>
+    </div>
+    <div className={styles.footer__generalLinks}>
+      <a href="http://www.gracebrunei.com/aboutus/">About</a>
+      <a href="#">Contact Us</a>
+      <a href="http://www.gracebrunei.com/tours/">Tours</a>
+    </div>
+    <p className={styles.footer__copyright}>
+      Grace Travel Agnecy is not responsible for content on external Websites.{" "}
+      <br />
+      Copyright 2019 Grace Travel Agency. All rights reserved.
+    </p>
+  </footer>
+)
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
 import Nav from "../components/nav"
+import Footer from "../components/footer"
 
 export default () => (
     <div className="mobile-container">
       <Nav />
+      <Footer />
     </div>
 )
