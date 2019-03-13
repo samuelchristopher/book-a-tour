919915020c60cf63699c1f58a0d8396c2998d3ed
Samuel Paul Christopher <thesamchris@gmail.com>
2019-03-13 13:48:55 +0800

    static version - part 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,30 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div class="mobile-container">
+      <div class="mobile-nav"><a class="logo" href="/"></a><a class="menu-button" href="#"></a></div>
+      <header>
+        <h1 class="shifted-to-the-right">Thank you for <br/>choosing<br/> Grace Travel <br/> Agency</h1>
+        <p class="thank-you__tagline shifted-to-the-right tagline">
+          We will be in touch with you within 24 hours, we look forward to taking you on the tour!
+          
+        </p>
+      </header>
+      <div class="thank-you__logo"></div>
+      <footer>
+        <div class="footer__logo"></div>
+        <div class="footer__partners-container">
+          <p class="footer__partners-heading">grace travel <br/>agency partners</p>
+          <div class="footer__partners-links-container">
+            <div class="partners-links__row"><a href="http://www.gracebrunei.com/clubmed/">Club Med</a><a href="http://www.gracebrunei.com/koreanair/">Korean Air</a></div>
+            <div class="partners-links__row"><a href="http://www.gracebrunei.com/bangkokhospital/">Bangkok Hospital</a><a href="http://www.gracebrunei.com/bangkokairways/">Bangkok Air</a></div>
+          </div>
+        </div>
+        <div class="footer__general-links"><a href="http://www.gracebrunei.com/aboutus/">About</a><a href="#">Contact Us</a><a href="http://www.gracebrunei.com/tours/">Tours</a></div>
+        <p class="footer__copyright">
+          Grace Travel Agnecy is not responsible for content on external Websites. <br/> 
+          Copyright 2019 Grace Travel Agency. All rights reserved.
+        </p>
+      </footer>
+    </div>
+)
