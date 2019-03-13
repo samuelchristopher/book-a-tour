442e5bfb4dc089326f6c06886480af18ab3f8155
Samuel Paul Christopher <thesamchris@gmail.com>
2019-03-13 14:36:48 +0800

    static: added nav

---
+++ src/components/nav.js
@@ -0,0 +1,13 @@
+import React from "react"
+import styles from "./nav.module.css"
+
+const Nav = () => {
+    return (
+      <div className="mobile-nav">
+        <a className={styles.logo} href="/" />
+        <a className={styles.menuButton} href="#" />
+      </div>
+    )
+}
+
+export default Nav
