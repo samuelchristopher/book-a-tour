867f8e219db2c14b0e67d0ca28dd1c276e05ad46
Samuel Paul Christopher <thesamchris@gmail.com>
2019-03-13 15:48:04 +0800

    simple validation for home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styles from "./home.module.css"
 
-export default () => (
+export default ({ isDisabled, handleInputChange }) => (
     <header className={styles.home__container}>
         <div className={styles.home__heroContainer}>
             <h1>Grace <br /> Travel <br /> Agnecy <br /> Tours
@@ -10,12 +10,12 @@ export default () => (
         </div>
         <p className="tagline">You are on your way to booking a tour! <br /> Please fill in the details below</p>
         <div className={styles.home__inputContainer}>
-            <input type="number" name="adults" placeholder="How many adults?" />
-            <input type="number" name="children" placeholder="How many children?" />
-            <input type="number" name="days" placeholder="How many days?" />
+            <input onChange={handleInputChange} type="number" name="adults" placeholder="How many adults?" />
+            <input onChange={handleInputChange} type="number" name="children" placeholder="How many children?" />
+            <input onChange={handleInputChange} type="number" name="days" placeholder="How many days?" />
         </div>
         <div className="center-button">
-            <button onClick="#">view tours</button>
+            <button disabled={isDisabled} >view tours</button>
         </div>
     </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,33 @@ import Layout from "../components/layout"
 import Home from "../components/home"
 
 class Index extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      adults: 0,
+      children: 0,
+      days: 0
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  handleInputChange(e) {
+    
+
+    let { name, value } = e.target
+    this.setState({
+      [name]: value
+    })
+    
+  }
+
   render() {
+    let { adults, children, days } = this.state
+    const isDisabled = !adults || !children || !days
     return (
       <Layout>
-        <Home />
+        <Home isDisabled={isDisabled} handleInputChange={this.handleInputChange} adults={this.state.adults} children={this.state.children} days={this.state.days}/>
       </Layout>
     )
   }
