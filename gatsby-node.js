665f76ea75267c683337c73e2a56372a225903b8
Samuel Paul Christopher <thesamchris@gmail.com>
2019-03-14 10:54:13 +0800

    added path

---
+++ gatsby-node.js
@@ -0,0 +1,33 @@
+const path = require("path")
+
+exports.createPages = ({ actions, graphql }) => {
+    const { createPages } = actions
+
+    const tourTemplate = path.resolve(`src/templates/tourTemplate.js`)
+
+    return graphql(`
+        {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            path
+                        }
+                    }
+                }
+            }
+        }
+    `).then(result => {
+        if(result.errors) {
+            return Promise.reject(result.errors)
+        }
+
+        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+            createPage({
+                path: node.frontmatter.path,
+                component: tourTemplate,
+                context: {}
+            })
+        })
+    })
+}
--- src/templates/tourTemplate.js
+++ src/templates/tourTemplate.js
@@ -1,25 +1,25 @@
-import React from "react"
-import { graphql } from "gatsby"
+// import React from "react"
+// import { graphql } from "gatsby"
 
-export default function Template({ data }) {
-    const { markdownRemark } = data
-    const { frontmatter, html } = markdownRemark
-    console.log(frontmatter, html)
-    return (
-        <div>
-            <h1>{frontmatter.title}</h1>
-        </div>
-    )
-}
+// export default function Template({ data }) {
+//     const { markdownRemark } = data
+//     const { frontmatter, html } = markdownRemark
+//     console.log(frontmatter, html)
+//     return (
+//         <div>
+//             <h1>{frontmatter.title}</h1>
+//         </div>
+//     )
+// }
 
-export const pageQuery = graphql`
-    query($path: String!) {
-        markdownRemark(frontmatter: { path: { eq: $path } }) {
-            html
-            frontmatter {
-                path
-                title
-            }
-        }
-    }
-`
+// export const pageQuery = graphql`
+//     query($path: String!) {
+//         markdownRemark(frontmatter: { slug: { eq: $path } }) {
+//             html
+//             frontmatter {
+//                 path
+//                 title
+//             }
+//         }
+//     }
+// `
