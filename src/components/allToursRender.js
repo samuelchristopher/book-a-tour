0113e4a8f4eb9d26540d91728363228239807051
Samuel Paul Christopher <thesamchris@gmail.com>
2019-03-15 09:09:38 +0800

    image one

--- src/components/allToursRender.js
+++ src/components/allToursRender.js
@@ -4,7 +4,24 @@ import TourCard from "./tourCard"
 
 const allToursRender = ({ data }) => {
     let tourCards = data.allMarkdownRemark.edges.map((tourCard, key) => {
-        return <TourCard title={tourCard.node.frontmatter.title} link={tourCard.node.frontmatter.path} key={key} />
+        if (tourCard.node.frontmatter.imgone) {
+            return (
+              <TourCard
+                title={tourCard.node.frontmatter.title}
+                link={tourCard.node.frontmatter.path}
+                key={key}
+                img={tourCard.node.frontmatter.imgone}
+              />
+            )
+        }
+        return (
+          <TourCard
+            title={tourCard.node.frontmatter.title}
+            link={tourCard.node.frontmatter.path}
+            key={key}
+            img="https://images.unsplash.com/photo-1551446591-142875a901a1?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=750&amp;q=80"
+          />
+        )
     })
 
     return (
