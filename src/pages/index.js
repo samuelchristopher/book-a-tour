e222cd9006df60145e9eeca1086e2cd82d28975c
Samuel Paul Christopher <thesamchris@gmail.com>
2019-03-18 11:47:15 +0800

    added page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout"
 import Home from "../components/home"
 import Results from "../components/results"
 import AllTours from "../components/allTours"
+import { Helmet } from "react-helmet"
 
 class Index extends React.Component {
   constructor() {
@@ -29,6 +30,10 @@ class Index extends React.Component {
     const isDisabled = !adults || !children || !days
     return (
       <Layout>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Tours | Grace Travel</title>
+        </Helmet>
         {/* <Home isDisabled={isDisabled} handleInputChange={this.handleInputChange} adults={this.state.adults} children={this.state.children} days={this.state.days}/> */}
         {/* <Results days={ days }/> */}
         <AllTours />
--- src/templates/tourTemplate.js
+++ src/templates/tourTemplate.js
@@ -2,6 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Details from "../components/details"
+import { Helmet } from "react-helmet"
 
 export default function Template({ data }) {
     const { markdownRemark } = data
@@ -10,6 +11,10 @@ export default function Template({ data }) {
     const { title, starts, ends, destinations, highlights, minage, mingroup, travelstyle, itinerary_url: itineraryUrl, itinerary: itineraryList, imgone, included, not_included: notIncluded, price, imgtwo, imgthree } = frontmatter
     return (
         <Layout>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>{ title } | Grace Travel</title>
+            </Helmet>
             <Details imgtwo={imgtwo} imgthree={imgthree} price={price} included={included} notIncluded={notIncluded} imgone={imgone} itineraryList={itineraryList} itineraryUrl={ itineraryUrl } minage={ minage } mingroup={ mingroup } travelstyle={ travelstyle } starts={ starts } title={ title } ends={ ends } destinations={ destinations } highlights={highlights}/>
         </Layout>
     )
