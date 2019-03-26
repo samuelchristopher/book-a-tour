406b32f2075375844ed5a60511eb6236dd24d90d
Samuel Paul Christopher <thesamchris@gmail.com>
2019-03-26 10:22:33 +0800

    temp centered menu

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,9 +4,13 @@ import styles from "./nav.module.css"
 const Nav = () => {
     return (
       <div className="mobile-nav">
-        <a className={styles.logo} href="/" />
-        <a className={styles.menuButton} href="#" />
+        <a className={styles.logo} style={{ margin: '0 auto' }}href="/" />
+        {/* <a className={styles.menuButton} href="#" /> */}
       </div>
+      // <div className="mobile-nav">
+      //   <a className={styles.logo} href="/" />
+      //   <a className={styles.menuButton} href="#" />
+      // </div>
     )
 }
 
