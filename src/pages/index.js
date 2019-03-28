4672a0600b316b1efbace027deb7f66b01593d88
Samuel Paul Christopher <thesamchris@gmail.com>
2019-03-28 09:02:05 +0800

    complete reset

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,25 @@
 import React from "react"
-import Layout from "../components/layout"
-// import Home from "../components/home"
-// import Results from "../components/results"
-import AllTours from "../components/allTours"
+
 import { Helmet } from "react-helmet"
 
 class Index extends React.Component {
   constructor() {
     super()
-    this.state = {
-      adults: 0,
-      children: 0,
-      days: 0
-    }
 
-    this.handleInputChange = this.handleInputChange.bind(this)
   }
 
-  handleInputChange(e) {
-    let { name, value } = e.target
-    this.setState({
-      [name]: value
-    })
-    
-  }
+
 
   render() {
-    let { adults, children, days } = this.state
-    const isDisabled = !adults || !children || !days
+
     return (
-      <Layout>
+      <div>
         <Helmet>
           <meta charSet="utf-8" />
           <title>Tours | Grace Travel</title>
         </Helmet>
-        {/* <Home isDisabled={isDisabled} handleInputChange={this.handleInputChange} adults={this.state.adults} children={this.state.children} days={this.state.days}/> */}
-        {/* <Results days={ days }/> */}
-        {/* <AllTours /> */}
-      </Layout>
+        <h1>hello</h1>
+      </div>
     )
   }
 }
