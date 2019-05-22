eb1cecfbb15383ff9161949a9fe404b53491a16e
Samuel Paul Christopher <thesamchris@gmail.com>
2019-05-22 10:53:39 +0800

    update thank you page temp

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -11,7 +11,7 @@ const ThankYou = () => {
                 <title>Thank You | Grace Travel</title>
             </Helmet>
             <header>
-                <h1 className="shifted-to-the-right">Thank you for <br />choosing<br /> Grace Travel <br /> Agency</h1>
+                <h1 className="shifted-to-the-right">Thank you for choosing Grace Travel Agency</h1>
                 <p className={`${styles.thankYou__tagline} shifted-to-the-right tagline`}>We will be in touch with you within 24 hours, we look forward to taking you on the tour!</p>
             </header>
             <div className={styles.thankYou__logo}></div>
